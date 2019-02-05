1b57e45714139c53a9a210eae0f834011de4b12b
whjin <1414848321@qq.com>
2019-02-05 20:52:49 +0800

    :memo:更新文档

--- src/component/CommentInput.js
+++ src/component/CommentInput.js
@@ -1,12 +1,40 @@
 import React, {Component} from 'react';
+import PropTypes from 'prop-types';
 
 class CommentInput extends Component {
+    static propTypes = {
+        onSubmit: PropTypes.func
+    };
+
     constructor() {
         super();
         this.state = {
             username: '',
             content: ''
         }
+    }
+
+    componentWillMount() {
+        this._loadUsername()
+    }
+
+    componentDidMount() {
+        this.textarea.focus()
+    }
+
+    _loadUsername() {
+        const username = localStorage.getItem('username');
+        if (username) {
+            this.setState({username})
+        }
+    }
+
+    _saveUsername(username) {
+        localStorage.setItem('username', username)
+    }
+
+    handleUsernameBlur(e) {
+        this._saveUsername(e.target.value)
     }
 
     handleUsernameChange(e) {
@@ -35,13 +63,16 @@ class CommentInput extends Component {
                 <div className="comment-field">
                     <span className="comment-field-name">用户名：</span>
                     <div className="comment-field-input">
-                        <input value={this.state.username} onChange={this.handleUsernameChange.bind(this)}/>
+                        <input onBlur={this.handleUsernameBlur.bind(this)} value={this.state.username}
+                               onChange={this.handleUsernameChange.bind(this)}/>
                     </div>
                 </div>
                 <div className="comment-field">
                     <span className="comment-field-name">评论内容：</span>
                     <div className="comment-field-input">
-                        <textarea value={this.state.content}
+                        <textarea ref={((textarea) => {
+                            this.textarea = textarea
+                        })} value={this.state.content}
                                   onChange={this.handleContentChange.bind(this)}/>
                     </div>
                 </div>
