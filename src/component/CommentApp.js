9710cb573bd3fd7453ef31567da28815c9cb034a
whjin <1414848321@qq.com>
2019-02-05 23:37:01 +0800

    :memo:更新文档

--- src/component/CommentApp.js
+++ src/component/CommentApp.js
@@ -10,20 +10,36 @@ class CommentApp extends Component {
         }
     }
 
+    componentWillMount() {
+        this._loadComments()
+    }
+
+    _loadComments() {
+        let comments = localStorage.getItem('comments');
+        if (comments) {
+            comments = JSON.parse(comments);
+            this.setState({comments})
+        }
+    }
+
+    _saveComments(comments) {
+        localStorage.setItem('comments', JSON.stringify(comments))
+    }
+
     handleSubmitComment(comment) {
-        if(!comment){
+        if (!comment) {
             return;
         }
-        if(!comment.username){
+        if (!comment.username) {
             return alert("请输入用户名！");
         }
-        if(!comment.content){
+        if (!comment.content) {
             return alert("请输入评论内容！")
         }
-        this.state.comments.push(comment);
-        this.setState({
-            comments:this.state.comments
-        })
+        const comments = this.state.comments;
+        comments.push(comment);
+        this.setState({comments});
+        this._saveComments(comments);
     }
 
     render() {
