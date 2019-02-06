95bffaa4009236badb3a4ad5b88122f147fbbc6a
whjin <1414848321@qq.com>
2019-02-06 18:33:45 +0800

    :memo:更新文档

--- src/component/Comment.js
+++ src/component/Comment.js
@@ -3,7 +3,9 @@ import PropTypes from 'prop-types';
 
 class Comment extends Component {
     static defaultProps = {
-        comment: PropTypes.object.isRequired
+        comment: PropTypes.object.isRequired,
+        onDeleteComment: PropTypes.func,
+        index: PropTypes.number
     };
 
     constructor() {
@@ -34,17 +36,35 @@ class Comment extends Component {
         })
     }
 
+    handleDeleteComment() {
+        if (this.props.onDeleteComment) {
+            this.props.onDeleteComment(this.props.index)
+        }
+    }
+
+    _getProcessedContent(content) {
+        return content
+            .replace(/&/g, "&amp;")
+            .replace(/</g, "&lt;")
+            .replace(/>/g, "&gt;")
+            .replace(/"/g, "quot;")
+            .replace(/'/g, "&#039;")
+            .replace(/`([\S\s]+?)`/g, '<code>$1</code>')
+    }
+
     render() {
         return (
             <div className="comment">
                 <div className="comment-user">
                     <span>{this.props.comment.username}</span>：
                 </div>
-                <p>{this.props.comment.content}</p>
+                <p dangerouslySetInnerHTML={{
+                    __html: this._getProcessedContent(this.props.comment.content)
+                }}/>
                 <span className="comment-createdtime">
                     {this.state.timeString}
                 </span>
-                <span className="comment-delete">删除</span>
+                <span onClick={this.handleDeleteComment.bind(this)} className="comment-delete">删除</span>
             </div>
         )
     }
