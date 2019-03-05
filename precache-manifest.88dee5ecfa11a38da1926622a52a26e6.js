2854b99ae76f0853b1dcc473af1eeb91239f959d
whjin <1414848321@qq.com>
2019-03-05 11:48:45 +0800

    Updates

---
+++ precache-manifest.88dee5ecfa11a38da1926622a52a26e6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "05e712e8e84531abfd0e",
+    "url": "/react-comment-app/static/js/runtime~main.05e712e8.js"
+  },
+  {
+    "revision": "8fd6be0a5c159a9a11cd",
+    "url": "/react-comment-app/static/js/main.8fd6be0a.chunk.js"
+  },
+  {
+    "revision": "d431d1cf2dc2190157f7",
+    "url": "/react-comment-app/static/js/2.d431d1cf.chunk.js"
+  },
+  {
+    "revision": "8fd6be0a5c159a9a11cd",
+    "url": "/react-comment-app/static/css/main.b1298562.chunk.css"
+  },
+  {
+    "revision": "13019809c3facc0ef5dba7d14bad123b",
+    "url": "/react-comment-app/index.html"
+  }
+];
